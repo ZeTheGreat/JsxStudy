b0dcbe9365da5f6a49a4a6a0a9b9d4c0569f6d5b
ZeTheGreat <junior.alves.5@hotmail.com>
2019-06-03 10:09:38 -0300

    First Commit

---
+++ src/AproovalCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const AproovalCards = (props) => {
+    return  (
+        <div className="ui cards">
+        <div className="card">
+                <div className="content">
+                <img className="right floated mini ui image" src="/images/avatar/large/elliot.jpg"/>
+                <div className="header">
+                  Elliot Fu
+            </div>
+            <div className="meta">
+                  Friends of Veronika
+            </div>
+            <div className="description">
+                  Elliot requested permission to view your contact details
+            </div>
+          </div>
+          <div className="extra content">
+                <div className="ui two buttons">
+                  <div className="ui basic green button">Approve</div>
+              <div className="ui basic red button">Decline</div>
+            </div>
+          </div>
+        </div>
+        </div>
+    )
+};
+export default AproovalCards;
